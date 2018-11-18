323744809a3328159dab733a8876ad32beee853e
lomodd <lomodd@126.com>
2018-11-18 14:50:19 +0800

    Updates

---
+++ precache-manifest.80c41679b528c4a261c2d84137705f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b1f294234fbd0dd3203",
+    "url": "/lomodd/static/css/main.522bfb1b.chunk.css"
+  },
+  {
+    "revision": "2b1f294234fbd0dd3203",
+    "url": "/lomodd/static/js/main.2b1f2942.chunk.js"
+  },
+  {
+    "revision": "1670935a042b1fdc38db",
+    "url": "/lomodd/static/js/1.1670935a.chunk.js"
+  },
+  {
+    "revision": "6be00d4b5212021b53fe",
+    "url": "/lomodd/static/js/runtime~main.6be00d4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lomodd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3a0ad4fc193d2360856eb18befc470d8",
+    "url": "/lomodd/index.html"
+  }
+];
